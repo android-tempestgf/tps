e442a7d7107fb916fc0a96353e1c15f34558e419
Tempestgf <tempestgf@protonmail.com>
2025-03-03 00:37:23 +0100

    v1.0.2

--- app/src/main/java/com/tempestgf/tps/AboutActivity.kt
+++ app/src/main/java/com/tempestgf/tps/AboutActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
